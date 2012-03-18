7820cb3373b2cc3714fa4ccf26ded165ea51f0ca
dan <daan.forever@gmail.com>
2012-03-18 20:53:48 +0400

    interm

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,29 @@
+
+require "bundler/capistrano"
+
 set :application, "webcrawler_rails"
 set :repository,  "git://github.com/daanforever/webcrawler_rails.git"
 
 set :scm, :git
-set :deploy_to, "/opt/capistrano"
+set :deploy_to, "/opt/capistrano/#{application}"
 
 role :web, "www.dron.me"
 role :app, "www.dron.me"
 role :db,  "www.dron.me", :primary => true
 
+namespace :deploy do
+  desc "Tell unicorn to restart the app."
+  task :restart do
+    run "#{current_path}/script/spin restart"
+  end
+
+  desc "Tell unicorn to stop the app."
+  task :stop do
+    run "#{current_path}/script/spin stop"
+  end
+
+  desc "Tell unicorn to start the app."
+  task :start do
+    run "#{current_path}/script/spin start"
+  end
+end
