8dc68ab41df1117bbfa5c8ded8d1618b88a5d0eb
dan <daan.forever@gmail.com>
2012-03-18 23:48:26 +0400

    unicorn listen socket fix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,12 +5,12 @@ preload_app true
 
 timeout 30
 
-listen "tmp/sockets/unicorn.sock", :backlog => 64
+listen "./tmp/sockets/unicorn.sock", :backlog => 64
 
-pid "tmp/pids/unicorn.pid"
+pid "./tmp/pids/unicorn.pid"
 
-stderr_path "log/unicorn.stderr.log"
-stdout_path "log/unicorn.stdout.log"
+stderr_path "./log/unicorn.stderr.log"
+stdout_path "./log/unicorn.stdout.log"
 
 before_fork do |server, worker|
     defined?(ActiveRecord::Base) and
